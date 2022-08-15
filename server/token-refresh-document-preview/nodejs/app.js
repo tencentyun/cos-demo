2e55ad010a62ae60ea23ac37f9363141f214ad86
xueyunzhou1 <110523274+xueyunzhou1@users.noreply.github.com>
2022-08-15 18:57:57 +0800

    Feature/token refresh (#3)
    * add:token续期demo
    Co-authored-by: zhouxueyun <zhouxueyun@tencent.com>

---
+++ server/token-refresh-document-preview/nodejs/app.js
@@ -0,0 +1,106 @@
+// token续期服务例子
+const express = require('express');
+const request = require('request');
+const util = require('./util');
+
+const config = {
+    // 获取腾讯云密钥，建议使用限定权限的子用户的密钥 https://console.cloud.tencent.com/cam/capi
+    secretId: "AKIxxxxxx",
+    secretKey: "xxxxxx",
+}
+
+const signExp = 10 * 60; // 签名过期时间，例如此处为10分钟
+
+// 创建token续期服务和用于调试的静态服务
+const app = express();
+
+// 获取签名接口
+app.all('/getSign', function (req, res, next) {
+    // 从请求中获取请求方法与请求参数
+    const { method, query: { filePath }} = req;
+    const pathname = filePath.substring(1);
+    // 计算签名需要的参数
+    const opt = {
+        SecretId: config.secretId,
+        SecretKey: config.secretKey,
+        method,
+        Pathname: pathname,
+        Expires: signExp,
+    }
+    // 计算得到签名
+    const sign = util.getAuth(opt);
+
+    // 如果需要返回跨域头
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', 'http://your.client.com:port'); // 这里修改允许跨域访问的网站
+    res.setHeader('Access-Control-Allow-Headers', 'origin,accept,content-type');
+
+    // 返回签名信息
+    res.send({
+        code: 0,
+        data: {
+            sign
+        },
+    });
+});
+
+// 获取 Token 接口
+app.all('/refreshToken', function (req, res, next) {
+    // 服务端在此处可以进行自己的逻辑处理，如鉴别客户端身份，是否继续提供token续期服务
+    /* Write your code here... */
+
+    // 从请求中获取参数
+    const { query: { Bucket, Region, filePath, tokenuid }} = req;
+
+    // 根据 Bucket, Region, filePath 拼接文件完整路径
+    const objectUrl = `https://${Bucket}.cos.${Region}.myqcloud.com${filePath}`;
+
+    // 计算签名需要的参数
+    const opt = {
+        SecretId: config.secretId,
+        SecretKey: config.secretKey,
+        method: "get",
+        Pathname: pathname,
+        Expires: signExp,
+    }
+    // 计算得到签名
+    const sign = util.getAuth(opt);
+
+    // 拼接getObject请求的url
+    const url = `${objectUrl}?ci-process=doc-preview&dstType=html&weboffice_url=1&tokenuid=${tokenuid}&sign=${util.camSafeUrlEncode(sign)}`;
+
+    // 发起getObject请求，获取 Token
+    request(url, (err, result) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        // IE返回的是string
+        if (typeof result.body === 'string') {
+            result.body = JSON.parse(result.body);
+        }
+        console.log(result.body)
+
+        // 从返回结果中获取 Token 和 Token过期时间戳
+        const Token = result.body && result.body.Token ? result.body.Token : '';
+        const TokenExpireTime = result.body && result.body.TokenExpireTime ? result.body.TokenExpireTime : '';
+
+        // 如果需要返回跨域头
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Access-Control-Allow-Origin', 'http://your.client.com:port'); // 这里修改允许跨域访问的网站
+        res.setHeader('Access-Control-Allow-Headers', 'origin,accept,content-type');
+
+        // 返回 Token 与 Token过期时间戳
+        res.send({
+            code: 0,
+            data: {
+                Token,
+                TokenExpireTime
+            },
+        });
+    });
+});
+
+// 启动服务端token续期服务
+app.listen(3000);
+console.log('app is listening at http://127.0.0.1:3000');
---
+++ server/token-refresh-document-preview/nodejs/util.js
@@ -0,0 +1,204 @@
+const crypto = require('crypto');
+
+const isArray = function(arr) {
+    return arr instanceof Array;
+};
+
+const clone = function(obj) {
+    return map(obj, function (v) {
+        return typeof v === 'object' && v !== null ? clone(v) : v;
+    });
+};
+
+const map = function(obj, fn) {
+    const o = isArray(obj) ? [] : {};
+    for (const i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            o[i] = fn(obj[i], i);
+        }
+    }
+    return o;
+};
+const signHeaders = ['content-disposition', 'content-encoding', 'content-length', 'content-md5',
+    'expect', 'host', 'if-match', 'if-modified-since', 'if-none-match', 'if-unmodified-since',
+    'origin', 'range', 'response-cache-control', 'response-content-disposition', 'response-content-encoding',
+    'response-content-language', 'response-content-type', 'response-expires', 'transfer-encoding', 'versionid'];
+
+const getSignHeaderObj = function (headers) {
+    const signHeaderObj = {};
+    for (const i in headers) {
+        const key = i.toLowerCase();
+        if (key.indexOf('x-cos-') > -1 || signHeaders.indexOf(key) > -1) {
+            signHeaderObj[i] = headers[i];
+        }
+    }
+    return signHeaderObj;
+}
+
+// 获取调正的时间戳
+const getSkewTime = function (offset) {
+    return Date.now() + (offset || 0);
+};
+
+const getObjectKeys = function (obj, forKey) {
+    const list = [];
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            list.push(forKey ? camSafeUrlEncode(key).toLowerCase() : key);
+        }
+    }
+    return list.sort(function (a, b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        return a === b ? 0 : (a > b ? 1 : -1);
+    });
+};
+
+const camSafeUrlEncode = function (str) {
+    return encodeURIComponent(str)
+        .replace(/!/g, '%21')
+        .replace(/'/g, '%27')
+        .replace(/\(/g, '%28')
+        .replace(/\)/g, '%29')
+        .replace(/\*/g, '%2A');
+}
+
+const obj2str = function (obj, lowerCaseKey) {
+    let i, key, val;
+    const list = [];
+    const keyList = getObjectKeys(obj);
+    for (i = 0; i < keyList.length; i++) {
+        key = keyList[i];
+        val = (obj[key] === undefined || obj[key] === null) ? '' : ('' + obj[key]);
+        key = lowerCaseKey? camSafeUrlEncode(key).toLowerCase() : camSafeUrlEncode(key);
+        val = camSafeUrlEncode(val) || '';
+        list.push(key + '=' + val)
+    }
+    return list.join('&');
+};
+
+//测试用的key后面可以去掉
+const getAuth = function (opt) {
+    opt = opt || {};
+
+    const SecretId = opt.SecretId;
+    const SecretKey = opt.SecretKey;
+    const KeyTime = opt.KeyTime;
+    const method = (opt.method || opt.Method || 'get').toLowerCase();
+    const queryParams = clone(opt.Query || opt.params || {});
+    const headers = getSignHeaderObj(clone(opt.Headers || opt.headers || {}));
+
+    const Key = opt.Key || '';
+    let pathname;
+    if (opt.UseRawKey) {
+        pathname = opt.Pathname || opt.pathname || '/' + Key;
+    } else {
+        pathname = opt.Pathname || opt.pathname || Key;
+        pathname.indexOf('/') !== 0 && (pathname = '/' + pathname);
+    }
+
+    // ForceSignHost明确传入false才不加入host签名
+    const forceSignHost = opt.ForceSignHost === false ? false : true;
+
+    // 如果有传入存储桶且需要强制签名，那么签名默认加 Host 参与计算，避免跨桶访问
+    if (!headers.Host && !headers.host && opt.Bucket && opt.Region && forceSignHost) headers.Host = opt.Bucket + '.cos.' + opt.Region + '.myqcloud.com';
+
+    if (!SecretId) throw new Error('missing param SecretId');
+    if (!SecretKey) throw new Error('missing param SecretKey');
+
+    // 签名有效起止时间
+    const now = Math.round(getSkewTime(opt.SystemClockOffset) / 1000) - 1;
+    let exp = now;
+
+    const Expires = opt.Expires || opt.expires;
+    if (Expires === undefined) {
+        exp += 900; // 签名过期时间为当前 + 900s
+    } else {
+        exp += (Expires * 1) || 0;
+    }
+
+    // 要用到的 Authorization 参数列表
+    const qSignAlgorithm = 'sha1';
+    const qAk = SecretId;
+    const qSignTime = KeyTime || now + ';' + exp;
+    const qKeyTime = KeyTime || now + ';' + exp;
+    const qHeaderList = getObjectKeys(headers, true).join(';').toLowerCase();
+    const qUrlParamList = getObjectKeys(queryParams, true).join(';').toLowerCase();
+
+    // 签名算法说明文档：https://www.qcloud.com/document/product/436/7778
+    // 步骤一：计算 SignKey
+    const signKey = crypto.createHmac('sha1', SecretKey).update(qKeyTime).digest('hex');
+
+    // 步骤二：构成 FormatString
+    const formatString = [method, pathname, obj2str(queryParams, true), obj2str(headers, true), ''].join('\n');
+
+    // 步骤三：计算 StringToSign
+    const stringToSign = ['sha1', qSignTime, crypto.createHash('sha1').update(formatString).digest('hex'), ''].join('\n');
+
+    // 步骤四：计算 Signature
+    const qSignature = crypto.createHmac('sha1', signKey).update(stringToSign).digest('hex');
+
+    // 步骤五：构造 Authorization
+    const authorization = [
+        'q-sign-algorithm=' + qSignAlgorithm,
+        'q-ak=' + qAk,
+        'q-sign-time=' + qSignTime,
+        'q-key-time=' + qKeyTime,
+        'q-header-list=' + qHeaderList,
+        'q-url-param-list=' + qUrlParamList,
+        'q-signature=' + qSignature
+    ].join('&');
+
+    return authorization;
+
+}
+
+const stringifyPrimitive = function (v) {
+    switch (typeof v ) {
+        case 'string':
+            return v;
+
+        case 'boolean':
+            return v ? 'true' : 'false';
+
+        case 'number':
+            return isFinite(v) ? v : '';
+
+        default:
+            return '';
+    }
+};
+
+const queryStringify = function (obj, sep, eq, name) {
+    sep = sep || '&';
+    eq = eq || '=';
+
+    if (obj === null) {
+        obj = undefined;
+    }
+
+    if (typeof obj === 'object') {
+        return Object.keys(obj).map((k) => {
+            const ks = encodeURIComponent(stringifyPrimitive(k)) + eq;
+
+            if (Array.isArray(obj[k])) {
+                return obj[k].map(v => ks + encodeURIComponent(stringifyPrimitive(v))).join(sep);
+            }
+
+            return ks + encodeURIComponent(stringifyPrimitive(obj[k]));
+        })
+            .filter(Boolean)
+            .join(sep);
+    }
+
+    if (!name) return '';
+    return encodeURIComponent(stringifyPrimitive(name)) + eq + encodeURIComponent(stringifyPrimitive(obj));
+};
+
+const util = {
+    getAuth,
+    queryStringify,
+    camSafeUrlEncode
+};
+
+module.exports = util;
