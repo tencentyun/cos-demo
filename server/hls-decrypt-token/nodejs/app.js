5f5c725451bfa554c86348c3d260c8d1769c4449
carsonxu <459452372@qq.com>
2024-03-06 18:27:41 +0800

    Update app.js

--- server/hls-decrypt-token/nodejs/app.js
+++ server/hls-decrypt-token/nodejs/app.js
@@ -8,7 +8,7 @@ const config = {
     // 获取腾讯云密钥，建议使用限定权限的子用户的密钥 https://console.cloud.tencent.com/cam/capi
     secretId: process.env.SecretId,
     secretKey: process.env.SecretKey,
-    // 播放秘钥，可通过接口获取或在控制台查看
+    // 播放秘钥，可通过到控制台（存储桶详情->数据处理->媒体处理）获取填写到这里，也可以调用万象 API 获取
     playerKey: process.env.playerKey
 };
 // 计算签名
@@ -128,7 +128,13 @@ router.post('/hls/token', (req, res, next) => {
     const body = req.body || {}
     const src = body.src;
     const publicKey = body.publicKey;
-    const protectContentKey = body.protectContentKey || 0;
+
+    // 代码示例只允许 protectContentKey 传 1，原因：如果允许传入 0 播放流程会走 HLS 标准加密会有风险。
+    // 如在某些特殊场景需要用HLS标准加密(例如小程序里播放)，可以去掉下面的限制判断并做好来源限制只允许小程序来源。
+    const protectContentKey = parseInt(body.protectContentKey || 0);
+    if (protectContentKey === 0) return res.send({code: 0, message: 'protectContentKey=0 not allowed'});
+
+    // src 链接校验
     const reg = /^https?:\/\/([a-z0-9-]+)\.cos\.([a-z0-9-]+)\.myqcloud\.com\/([^?]+)/;
     if (!reg.test(src)) return res.send({code: -1, message: 'src format error'});
     if (!publicKey) return res.send({code: -1, message: 'publicKey empty'});
