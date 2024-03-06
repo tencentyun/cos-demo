63ae5752ec7955ffa8a789e1d72f3b95a5f672c2
carsonxu <459452372@qq.com>
2024-03-06 19:13:28 +0800

    更新计算签名的代码

--- server/hls-decrypt-token/nodejs/app.js
+++ server/hls-decrypt-token/nodejs/app.js
@@ -1,4 +1,5 @@
 // HLS加解密服务例子
+const COS = require('cos-nodejs-sdk-v5');
 const base64Url = require('base64-url');
 const express = require('express');
 const crypto = require('crypto');
@@ -9,82 +10,9 @@ const config = {
     secretId: process.env.SecretId,
     secretKey: process.env.SecretKey,
     // 播放秘钥，可通过到控制台（存储桶详情->数据处理->媒体处理）获取填写到这里，也可以调用万象 API 获取
-    playerKey: process.env.playerKey
-};
-// 计算签名
-const getAuth = function (opt) {
-	opt = opt || {};
-
-	const secretId = opt.secretId;
-	const secretKey = opt.secretKey;
-	let method = opt.method || 'get';
-	method = method.toLowerCase();
-	const pathname = opt.pathname || '/';
-	const queryParams = opt.params || '';
-	const headers = opt.headers || '';
-	const getObjectKeys = function (obj) {
-		let list = [];
-		for (let key in obj) {
-			if (obj.hasOwnProperty(key)) {
-				list.push(key);
-			}
-		}
-		return list.sort();
-	};
-	const obj2str = function (obj) {
-		let i, key, val;
-		let list = [];
-		let keyList = Object.keys(obj);
-		for (i = 0; i < keyList.length; i++) {
-			key = keyList[i];
-			val = obj[key] || '';
-			key = key.toLowerCase();
-			key = encodeURIComponent(key);
-			list.push(key + '=' + encodeURIComponent(val));
-		}
-		return list.join('&');
-	};
-	// 签名有效起止时间
-	const now = parseInt(new Date().getTime() / 1000) - 1;
-	let expired = now; // now + ';' + (now + 60) + ''; // 签名过期时间为当前 + 3600s
-	if (opt.expires) {
-		expired += (opt.expires * 1);
-	} else {
-		expired += 3600;
-	}
-	// 要用到的 Authorization 参数列表
-	const qSignAlgorithm = 'sha1';
-	const qAk = secretId;
-	const qSignTime = now + ';' + expired;
-	const qKeyTime = now + ';' + expired;
-	const qHeaderList = getObjectKeys(headers).join(';').toLowerCase();
-	const qUrlParamList = getObjectKeys(queryParams).join(';').toLowerCase();
-	// 签名算法说明文档：https://www.qcloud.com/document/product/436/7778
-	// 步骤一：计算 SignKey
-	const signKey = crypto.createHmac('sha1', secretKey).update(qKeyTime).digest('hex');//CryptoJS.HmacSHA1(qKeyTime, secretKey).toString();
-	// 新增修改，formatString 添加 encodeURIComponent
-	//pathname = encodeURIComponent(pathname);
-	// 步骤二：构成 FormatString
-	let formatString = [method, pathname, obj2str(queryParams), obj2str(headers), ''].join('\n');
-	formatString = Buffer.from(formatString, 'utf8');
-	// 步骤三：计算 StringToSign
-	const sha1Algo = crypto.createHash('sha1');
-	sha1Algo.update(formatString);
-	const res = sha1Algo.digest('hex');
-	const stringToSign = ['sha1', qSignTime, res, ''].join('\n');
-	// 步骤四：计算 Signature
-	const qSignature = crypto.createHmac('sha1', signKey).update(stringToSign).digest('hex');//CryptoJS.HmacSHA1(stringToSign, signKey).toString();
-	// 步骤五：构造 Authorization
-	const authorization = [
-		'q-sign-algorithm=' + qSignAlgorithm,
-		'q-ak=' + qAk,
-		'q-sign-time=' + qSignTime,
-		'q-key-time=' + qKeyTime,
-		'q-header-list=' + qHeaderList,
-		'q-url-param-list=' + qUrlParamList,
-		'q-signature=' + qSignature
-	].join('&');
-	return authorization;
+    playerKey: process.env.playerKey,
+	// 默认不要允许标准加密播放，如在某些特殊场景需要用HLS标准加密(例如小程序里播放)，可以改为 true，去掉下面的限制判断并做好来源限制只允许小程序来源。
+	allowStandardEncrypt: false,
 };
 
 // 获取播放 token
@@ -112,14 +40,12 @@ function getToken({publicKey, protectContentKey, bucket, region, objectKey}) {
     let hash = crypto.createHmac('sha256', config.playKey).update(data).digest();
     let Signature = base64Url.encode(hash);
     let token = Header + '.' + PayLoad + '.' + Signature
-    let authorization = getAuth({
-        secretId: config.SecretId,
-        secretKey: config.SecretKey,
-        method: 'get',
-        pathname: `/${objectKey}`,
-        params: {
-            'ci-process': 'pm3u8'
-        },
+    let authorization = COS.getAuthorization({
+        SecretId: config.secretId,
+        SecretKey: config.secretKey,
+        Method: 'get',
+        Pathname: `/${objectKey}`,
+        Query: {'ci-process': 'pm3u8'},
     });
     return {token, authorization};
 }
@@ -136,7 +62,7 @@ app.post('/hls/token', (req, res, next) => {
     // 代码示例只允许 protectContentKey 传 1，原因：如果允许传入 0 播放流程会走 HLS 标准加密会有风险。
     // 如在某些特殊场景需要用HLS标准加密(例如小程序里播放)，可以去掉下面的限制判断并做好来源限制只允许小程序来源。
     const protectContentKey = parseInt(body.protectContentKey || 0);
-    if (protectContentKey === 0) {
+    if (!config.allowStandardEncrypt && !protectContentKey) {
 		res.status(400);
 		return res.send({code: -1, message: 'protectContentKey=0 not allowed'});
 	}
