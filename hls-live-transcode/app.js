f00eb974b292c7a1f155d074008623b28034ea60
carsonxu <459452372@qq.com>
2024-04-10 23:53:14 +0800

    补充 HLS 边转边播+私有加密示例

--- hls-live-transcode/app.js
+++ hls-live-transcode/app.js
@@ -3,54 +3,98 @@ const COS = require('cos-nodejs-sdk-v5');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const crypto = require("crypto");
+const base64Url = require('base64-url');
 
 // 配置参数
 const config = {
     // 获取腾讯云密钥，建议使用限定权限的子用户的密钥 https://console.cloud.tencent.com/cam/capi
-    secretId: 'xx',
-    secretKey: 'xx',
+    secretId: 'xxx',
+    secretKey: 'xxx',
+    // 播放秘钥，可通过到控制台（存储桶详情->数据处理->媒体处理）获取填写到这里，也可以调用万象 API 获取
+    playKey: 'xxx',
+    bucket: 'wwj-cq-1253960454',
+    region: 'ap-chongqing',
 };
-
-// 获取播放 Authorization
-function getAuthorization({objectKey}) {
-    let authorization = COS.getAuthorization({
-        SecretId: config.secretId,
-        SecretKey: config.secretKey,
-        Method: 'get',
-        Pathname: `/${objectKey}`,
-        Query: {'ci-process': 'getplaylist'},
-    });
-    return {authorization};
-}
 
 // 创建临时密钥服务和用于调试的静态服务
 const app = express();
 
-app.use(cors({ origin: '*', credentials: true }));
-// body-parser中间件
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// 获取播放签名及可播放链接
-app.post('/hls/getAuthorization', (req, res, next) => {
+// 获取播放 token
+const srcReg = /^https?:\/\/([^/]+)\/([^?]+)/;
+const ciHostReg = /^[^.]+\.ci\.[^.]+\.myqcloud\.com$/;
+function getToken({publicKey, protectContentKey, bucket, region, src}) {
+    const m = src.match(srcReg);
+    const srcHost = m[1];
+    const pathKey = m[2];
+    const query = {};
+    const isCiHost = ciHostReg.test(srcHost);
+    src.replace(/^([^?]+)(\?([^#]+))?(#.*)?$/, '$3').split('&').forEach(item => {
+        const index = item.indexOf('=');
+        const key = index > -1 ? item.slice(0, index) : item;
+        let val = index > -1 ? item.slice(index + 1) : '';
+        query[key] = decodeURIComponent(val);
+    });
+    let objectKey = isCiHost ? query.object : pathKey;
+    const header = {
+        "alg": "HS256",
+        "typ": "JWT"
+    }
+    const appId = bucket.slice(bucket.lastIndexOf('-') + 1);
+    let payload = {
+        Type: "CosCiToken",
+        AppId: appId,
+        BucketId: bucket,
+        Issuer: "client",
+        IssuedTimeStamp: Math.floor((new Date().getTime() - 30 * 1000) / 1000),
+        ProtectSchema: "rsa1024",
+        PublicKey: publicKey,
+        ProtectContentKey: protectContentKey || 0,
+        UsageLimit: 50,
+        Object: objectKey,
+    };
+    let Header = base64Url.encode(JSON.stringify(header))
+    let PayLoad = base64Url.encode(JSON.stringify(payload))
+    let data = Header + "." + PayLoad
+    let hash = crypto.createHmac('sha256', config.playKey).update(data).digest();
+    let Signature = base64Url.encode(hash);
+    let token = Header + '.' + PayLoad + '.' + Signature
+    const authOpt = {
+        SecretId: config.secretId,
+        SecretKey: config.secretKey,
+        Method: 'get',
+        Pathname: '/' + pathKey,
+    };
+    if (!isCiHost) {
+        const ciProcess = query['ci-process'] ||'pm3u8';
+        authOpt.Query = {'ci-process': ciProcess};
+    }
+    let authorization = COS.getAuthorization(authOpt);
+    return {token, authorization};
+}
+
+// 提供接口，给前端/App播放器，获取播放 token
+app.post('/hls/token', (req, res, next) => {
     const body = req.body || {}
     const src = body.src;
+    const publicKey = body.publicKey;
+    const protectContentKey = parseInt(body.protectContentKey || 0);
 
     // src 链接校验
-    const reg = /^https?:\/\/([a-z0-9-]+)\.cos\.([a-z0-9-]+)\.myqcloud\.com\/([^?]+)/;
-    if (!reg.test(src)) return res.send({code: -1, message: 'src format error'});
+    if (!src || !srcReg.test(src)) return res.send({code: -1, message: 'src format error'});
+    if (!publicKey) return res.send({code: -1, message: 'publicKey empty'});
 
     // 解析 url
-    const [, bucket, region, objectKey] = src.match(reg) || [];
+    const { bucket, region } = config;
+    const { token, authorization } = getToken({publicKey, protectContentKey, bucket, region, src}, res)
 
-    const {authorization} = getAuthorization({objectKey})
-    // 返回签名和播放视频链接
-    let signedM3u8Url = `${src}?ci-process=getplaylist&expires=43200&${authorization}`
-    res.send({code: 0, message: 'ok', authorization, signedM3u8Url});
+    res.send({code: 0, message: 'ok', token, authorization});
 });
 
 app.use(express.static('./www'));
-
 
 app.all('*', function (req, res, next) {
     res.send({code: -1, message: '404 Not Found'});
